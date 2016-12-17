86d07aaff905a1e3ffc8c05dedf63e53ac590cb1
Zoff <zoff99@users.noreply.github.com>
2016-12-17 16:13:04 +0100

    1

--- echobot/echo_bot.c
+++ echobot/echo_bot.c
@@ -18,6 +18,16 @@ typedef struct DHT_node {
     unsigned char key_bin[TOX_PUBLIC_KEY_SIZE];
 } DHT_node;
 
+#define MAX_AVATAR_FILE_SIZE 65536
+
+static struct Avatar {
+    char name[TOX_MAX_FILENAME_LENGTH + 1];
+    size_t name_len;
+    char path[PATH_MAX + 1];
+    size_t path_len;
+    off_t size;
+} Avatar;
+
 const char *savedata_filename = "savedata.tox";
 const char *savedata_tmp_filename = "savedata.tox.tmp";
 const char *log_filename = "echobot.log";
@@ -126,6 +136,19 @@ void friend_message_cb(Tox *tox, uint32_t friend_number, TOX_MESSAGE_TYPE type, 
     tox_friend_send_message(tox, friend_number, type, message, length, NULL);
 }
 
+void on_file_chunk_request(Tox *tox, uint32_t friendnumber, uint32_t filenumber, uint64_t position,
+                           size_t length, void *userdata)
+{
+
+    if (ft->file_type == TOX_FILE_KIND_AVATAR)
+    {
+        // not supported at the moment!
+        return;
+    }
+
+}
+
+
 void self_connection_status_cb(Tox *tox, TOX_CONNECTION connection_status, void *user_data)
 {
     switch (connection_status) {
@@ -140,6 +163,100 @@ void self_connection_status_cb(Tox *tox, TOX_CONNECTION connection_status, void 
             break;
     }
 }
+
+
+int avatar_send(Tox *m, uint32_t friendnum)
+{
+    TOX_ERR_FILE_SEND err;
+    uint32_t filenum = tox_file_send(m, friendnum, TOX_FILE_KIND_AVATAR, (size_t) Avatar.size,
+                                     NULL, (uint8_t *) Avatar.name, Avatar.name_len, &err);
+
+    if (Avatar.size == 0)
+        return 0;
+
+    if (err != TOX_ERR_FILE_SEND_OK) {
+        fprintf(stderr, "tox_file_send failed for friendnumber %d (error %d)\n", friendnum, err);
+        return -1;
+    }
+
+    struct FileTransfer *ft = new_file_transfer(NULL, friendnum, filenum, FILE_TRANSFER_SEND, TOX_FILE_KIND_AVATAR);
+
+    if (!ft)
+        return -1;
+
+    ft->file = fopen(Avatar.path, "r");
+
+    if (ft->file == NULL)
+        return -1;
+
+    snprintf(ft->file_name, sizeof(ft->file_name), "%s", Avatar.name);
+    ft->file_size = Avatar.size;
+
+    return 0;
+}
+
+off_t file_size(const char *path)
+{
+    struct stat st;
+
+    if (stat(path, &st) == -1)
+        return 0;
+
+    return st.st_size;
+}
+
+int check_file_signature(const char *signature, size_t size, FILE *fp)
+{
+    char buf[size];
+    if (fread(buf, size, 1, fp) != 1)
+    {
+        return -1;
+    }
+    int ret = memcmp(signature, buf, size);
+    if (fseek(fp, 0L, SEEK_SET) == -1)
+    {
+        return -1;
+    }
+    return ret == 0 ? 0 : 1;
+}
+
+int avatar_set(Tox *m, const char *path, size_t path_len)
+{
+    if (path_len == 0 || path_len >= sizeof(Avatar.path))
+    {
+        return -1;
+    }
+
+    FILE *fp = fopen(path, "rb");
+    if (fp == NULL)
+    {
+        return -1;
+    }
+    char PNG_signature[8] = {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
+    if (check_file_signature(PNG_signature, sizeof(PNG_signature), fp) != 0) {
+        fclose(fp);
+        return -1;
+    }
+    fclose(fp);
+
+    off_t size = file_size(path);
+
+    if (size == 0 || size > MAX_AVATAR_FILE_SIZE)
+    {
+        return -1;
+    }
+
+    get_file_name(Avatar.name, sizeof(Avatar.name), path);
+    Avatar.name_len = strlen(Avatar.name);
+    snprintf(Avatar.path, sizeof(Avatar.path), "%s", path);
+    Avatar.path_len = path_len;
+    Avatar.size = size;
+
+    avatar_send_all(m);
+
+    return 0;
+}
+
 
 int main()
 {
@@ -158,13 +275,20 @@ int main()
 
     print_tox_id(tox);
 
+    // init callbacks ----------------------------------
     tox_callback_friend_request(tox, friend_request_cb);
     tox_callback_friend_message(tox, friend_message_cb);
-
+    tox_callback_file_chunk_request(tox, on_file_chunk_request);
     tox_callback_self_connection_status(tox, self_connection_status_cb);
+    // init callbacks ----------------------------------
 
     update_savedata_file(tox);
 
+    char path[300];
+    snprintf(path, sizeof(path), "%s", "./avatar.png");
+    int len = strlen(path) - 1;
+    avatar_set(tox, path, len);
+    
     while (1)
     {
         tox_iterate(tox, NULL);
