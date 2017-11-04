17d722a654c3dcace15c4fab7cb3cf0aed609122
Zoff <zoff99@users.noreply.github.com>
2017-11-04 22:41:14 +0100

    VP8E_SET_CPUUSED_VALUE (10) , g_lag_in_frames = 3 , MAX_DECODE_TIME_US VPX_DL_REALTIME

--- toxav/video.c
+++ toxav/video.c
@@ -37,7 +37,7 @@
   Soft deadline the decoder should attempt to meet, in us (microseconds). Set to zero for unlimited.
   By convention, the value 1 is used to mean "return as fast as possible."
 */
-#define MAX_DECODE_TIME_US VPX_DL_GOOD_QUALITY
+#define MAX_DECODE_TIME_US VPX_DL_REALTIME
 /*
 VPX_DL_REALTIME       (1)
 deadline parameter analogous to VPx REALTIME mode.
@@ -49,7 +49,7 @@ VPX_DL_BEST_QUALITY   (0)
 deadline parameter analogous to VPx BEST QUALITY mode.
 */
 
-#define VP8E_SET_CPUUSED_VALUE (3)
+#define VP8E_SET_CPUUSED_VALUE (10)
 /*
 Codec control function to set encoder internal speed settings.
 Changes in this value influences, among others, the encoder's selection of motion estimation methods.
@@ -129,7 +129,7 @@ VCSession *vc_new(Logger *log, ToxAV *av, uint32_t friend_number, toxav_video_re
 
     /* zoff (in 2017) */
     cfg.g_error_resilient = VPX_ERROR_RESILIENT_DEFAULT | VPX_ERROR_RESILIENT_PARTITIONS;
-    cfg.g_lag_in_frames = 1;
+    cfg.g_lag_in_frames = 3;
   /* Allow lagged encoding
    *
    * If set, this value allows the encoder to consume a number of input
