c42f4c961ff23ed4c0760208bf042c50af63930d
zoff99 <zoff@zoff.cc>
2017-11-19 18:37:17 +0100

    revert MAX_CRYPTO_VIDEO_DATA_SIZE

--- toxav/rtp.h
+++ toxav/rtp.h
@@ -28,8 +28,7 @@
 #include <stdbool.h>
 
 /* Max size of data in packets for VIDEO !! */
-// #define MAX_CRYPTO_VIDEO_DATA_SIZE (MAX_CRYPTO_DATA_SIZE / 2)
-#define MAX_CRYPTO_VIDEO_DATA_SIZE (MAX_CRYPTO_DATA_SIZE)
+#define MAX_CRYPTO_VIDEO_DATA_SIZE MAX_CRYPTO_DATA_SIZE
 
 /**
  * Payload type identifier. Also used as rtp callback prefix.
