be81e9fe4d303f1948fb78f976ef3e44968ba0bb
zoff99 <dummy@vps263487.ovh.net>
2017-12-11 13:41:42 +0100

    formatting corrected

--- toxav/toxav.c
+++ toxav/toxav.c
@@ -1080,7 +1080,7 @@ static void callback_bwc(BWController *bwc, uint32_t friend_number, float loss, 
 
     LOGGER_API_DEBUG(call->av->tox, "Reported loss of %f%%", (double)loss * 100);
 
-	/* if less than 10% data loss we do nothing! */
+    /* if less than 10% data loss we do nothing! */
     if (loss < 0.1f) {
         return;
     }
