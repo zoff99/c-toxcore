57bc310fc1c7dd8bf72b88c34353dad01d68e1f7
zoff99 <zoff@zoff.cc>
2017-12-19 20:34:45 +0100

    WIP: audio sync

--- toxav/toxav.c
+++ toxav/toxav.c
@@ -41,7 +41,7 @@ VPX_DL_BEST_QUALITY   (0)       deadline parameter analogous to VPx BEST QUALITY
 */
 
 #define VIDEO_ACCEPTABLE_LOSS (0.08f) /* if loss is less than this (8%), then don't do anything */
-#define AUDIO_ITERATATIONS_WHILE_VIDEO (5)
+#define AUDIO_ITERATATIONS_WHILE_VIDEO (0)
 
 #if defined(AUDIO_DEBUGGING_SKIP_FRAMES)
 uint32_t _debug_count_sent_audio_frames = 0;
@@ -310,6 +310,7 @@ void toxav_iterate(ToxAV *av)
 
             ac_iterate(i->audio);
             vc_iterate(i->video);
+
 
             if (i->msi_call->self_capabilities & msi_CapRAudio &&
                     i->msi_call->peer_capabilities & msi_CapSAudio) {
