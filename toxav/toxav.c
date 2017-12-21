95b7323e471f4c0117d49deb24445d1178172da6
zoff99 <zoff@zoff.cc>
2017-12-21 19:36:19 +0100

    WIP: audio

--- toxav/toxav.c
+++ toxav/toxav.c
@@ -77,6 +77,11 @@ typedef struct ToxAVCall_s {
     uint64_t last_incoming_audio_frame_rtimestamp;
     uint64_t last_incoming_audio_frame_ltimestamp;
 
+    uint64_t reference_rtimestamp;
+    uint64_t reference_ltimestamp;
+    int64_t reference_diff_timestamp;
+    uint8_t reference_diff_timestamp_set;
+
     /** Required for monitoring changes in states */
     uint8_t previous_self_capabilities;
 
@@ -335,24 +340,39 @@ void toxav_iterate(ToxAV *av)
             &&
             (i->last_incoming_video_frame_ltimestamp != 0))
             {
-                int64_t latency_ms = (
-                (i->last_incoming_video_frame_rtimestamp - i->last_incoming_audio_frame_rtimestamp) -
-                (i->last_incoming_video_frame_ltimestamp - i->last_incoming_audio_frame_ltimestamp)
-                );
+                if (i->reference_diff_timestamp_set == 0)
+                {
+                    i->reference_rtimestamp = i->last_incoming_audio_frame_rtimestamp;
+                    i->reference_ltimestamp = i->last_incoming_audio_frame_ltimestamp;
+                    // this is the difference between local and remote clocks in "ms"
+                    i->reference_diff_timestamp = (int64_t)(i->reference_ltimestamp - i->reference_rtimestamp);
+                    i->reference_diff_timestamp_set = 1;
+                }
+                else
+                {                
+                    int64_t latency_ms = (int64_t)(
+                    (i->last_incoming_video_frame_rtimestamp - i->last_incoming_audio_frame_rtimestamp) -
+                    (i->last_incoming_video_frame_ltimestamp - i->last_incoming_audio_frame_ltimestamp)
+                    );
+                    
 
-                LOGGER_INFO(av->m->log, "VIDEO:1-latency-in-ms=%lld", (long long)latency_ms);
+                    LOGGER_INFO(av->m->log, "VIDEO:delay-to-audio-in-ms=%lld", (long long)latency_ms);
+                    // LOGGER_INFO(av->m->log, "CLOCK:delay-to-rmote-in-ms=%lld", (long long)(i->reference_diff_timestamp));
+                    LOGGER_INFO(av->m->log, "VIDEO:delay-to-refnc-in-ms=%lld", (long long)-((i->last_incoming_video_frame_ltimestamp - i->reference_diff_timestamp) - i->last_incoming_video_frame_rtimestamp));
+                    LOGGER_INFO(av->m->log, "AUDIO:delay-to-refnc-in-ms=%lld", (long long)-((i->last_incoming_audio_frame_ltimestamp - i->reference_diff_timestamp) - i->last_incoming_audio_frame_rtimestamp));
 
-                LOGGER_INFO(av->m->log, "VIDEO latency in ms=%lld", (long long)(i->last_incoming_video_frame_ltimestamp - i->last_incoming_video_frame_rtimestamp));
-                LOGGER_INFO(av->m->log, "AUDIO latency in ms=%lld", (long long)(i->last_incoming_audio_frame_ltimestamp - i->last_incoming_audio_frame_rtimestamp));
+                    // LOGGER_INFO(av->m->log, "VIDEO latency in ms=%lld", (long long)(i->last_incoming_video_frame_ltimestamp - i->last_incoming_video_frame_rtimestamp));
+                    // LOGGER_INFO(av->m->log, "AUDIO latency in ms=%lld", (long long)(i->last_incoming_audio_frame_ltimestamp - i->last_incoming_audio_frame_rtimestamp));
 
-                LOGGER_INFO(av->m->log, "VIDEO:3-latency-in-ms=%lld", (long long)(i->last_incoming_audio_frame_rtimestamp - i->last_incoming_video_frame_rtimestamp));
+                    // LOGGER_INFO(av->m->log, "VIDEO:3-latency-in-ms=%lld", (long long)(i->last_incoming_audio_frame_rtimestamp - i->last_incoming_video_frame_rtimestamp));
 
-                LOGGER_INFO(av->m->log, "AUDIO (to video):latency in a=%lld b=%lld c=%lld d=%lld",
-                (long long)i->last_incoming_video_frame_rtimestamp,
-                (long long)i->last_incoming_audio_frame_rtimestamp,
-                (long long)i->last_incoming_video_frame_ltimestamp,
-                (long long)i->last_incoming_audio_frame_ltimestamp
-                );
+                    //LOGGER_INFO(av->m->log, "AUDIO (to video):latency in a=%lld b=%lld c=%lld d=%lld",
+                    //(long long)i->last_incoming_video_frame_rtimestamp,
+                    //(long long)i->last_incoming_audio_frame_rtimestamp,
+                    //(long long)i->last_incoming_video_frame_ltimestamp,
+                    //(long long)i->last_incoming_audio_frame_ltimestamp
+                    //);
+                }
             }            
         }
     }
@@ -1341,6 +1361,11 @@ static ToxAVCall *call_new(ToxAV *av, uint32_t friend_number, Toxav_Err_Call *er
 
     call->last_incoming_audio_frame_rtimestamp = 0;
     call->last_incoming_audio_frame_ltimestamp = 0;
+    
+    call->reference_rtimestamp = 0;
+    call->reference_ltimestamp = 0;
+    call->reference_diff_timestamp = 0;
+    call->reference_diff_timestamp_set = 0;
 
     if (call == NULL) {
         rc = TOXAV_ERR_CALL_MALLOC;
--- toxav/video.c
+++ toxav/video.c
@@ -636,14 +636,13 @@ void video_switch_decoder(VCSession *vc)
 }
 
 
-void vc_iterate(VCSession *vc, uint8_t skip_video_flag, uint64_t *a_r_timestamp, uint64_t *a_l_timestamp, uint64_t *v_r_timestamp, uint64_t *v_l_timestamp)
+uint8_t vc_iterate(VCSession *vc, uint8_t skip_video_flag, uint64_t *a_r_timestamp, uint64_t *a_l_timestamp, uint64_t *v_r_timestamp, uint64_t *v_l_timestamp)
 {
     if (!vc) {
-        return;
+        return 0;
     }
 
-    pthread_mutex_lock(vc->queue_mutex);
-
+	uint8_t ret_value = 0;
     struct RTPMessage *p;
 
     if (!rb_read(vc->vbuf_raw, (void **)&p)) {
@@ -741,6 +740,8 @@ void vc_iterate(VCSession *vc, uint8_t skip_video_flag, uint64_t *a_r_timestamp,
 						LOGGER_ERROR(vc->log, "VIDEO:TTx: %llu now=%llu", frame_record_timestamp_vpx, current_time_monotonic());
 						if (frame_record_timestamp_vpx > 0)
 						{
+							ret_value = 1;
+							
 							if (*v_r_timestamp < frame_record_timestamp_vpx)
 							{
 								LOGGER_ERROR(vc->log, "VIDEO:TTx:2: %llu", frame_record_timestamp_vpx);
@@ -775,6 +776,10 @@ void vc_iterate(VCSession *vc, uint8_t skip_video_flag, uint64_t *a_r_timestamp,
         // no frame data available
         // LOGGER_WARNING(vc->log, "Error decoding video: rb_read");
     }
+
+    pthread_mutex_unlock(vc->queue_mutex);
+    
+    return ret_value;
 }
 
 
