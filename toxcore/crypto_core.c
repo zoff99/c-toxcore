1357cfe8a33a540ebd12690da1fc56f1252ac9eb
zoff99 <zoff@zoff.cc>
2018-11-01 20:20:09 +0100

    fix 2nd VLA crash

--- toxcore/crypto_core.c
+++ toxcore/crypto_core.c
@@ -170,8 +170,8 @@ int32_t decrypt_data_symmetric(const uint8_t *secret_key, const uint8_t *nonce,
         return -1;
     }
 
-    VLA(uint8_t, temp_plain, length + crypto_box_ZEROBYTES);
-    VLA(uint8_t, temp_encrypted, length + crypto_box_BOXZEROBYTES);
+    uint8_t *temp_plain = calloc(1, length + crypto_box_ZEROBYTES);
+    uint8_t *temp_encrypted = calloc(1, length + crypto_box_MACBYTES + crypto_box_BOXZEROBYTES);
 
     memset(temp_encrypted, 0, crypto_box_BOXZEROBYTES);
     // Pad the message with 16 0 bytes.
@@ -179,10 +179,14 @@ int32_t decrypt_data_symmetric(const uint8_t *secret_key, const uint8_t *nonce,
 
     if (crypto_box_open_afternm(temp_plain, temp_encrypted, length + crypto_box_BOXZEROBYTES, nonce,
                                 secret_key) != 0) {
+        free(temp_plain);
+        free(temp_encrypted);
         return -1;
     }
 
     memcpy(plain, temp_plain + crypto_box_ZEROBYTES, length - crypto_box_MACBYTES);
+    free(temp_plain);
+    free(temp_encrypted);
     return length - crypto_box_MACBYTES;
 }
 
