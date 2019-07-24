eabd731bfc66136becab1b4d382923a059fa1381
zoff99 <zoff@zoff.cc>
2019-07-24 17:56:35 +0200

    remove old include

--- toxav/ring_buffer.c
+++ toxav/ring_buffer.c
@@ -19,7 +19,6 @@
  * You should have received a copy of the GNU General Public License
  * along with Tox.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "ccompat.h"
 #include "../toxcore/ccompat.h"
 
 #include <stdlib.h>
