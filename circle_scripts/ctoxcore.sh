565971c7757612985a352953b7b725e1908cc9d0
zoff99 <zoff@zoff.cc>
2019-08-03 16:35:32 +0200

    ignore -Wformat= errors

--- circle_scripts/ctoxcore.sh
+++ circle_scripts/ctoxcore.sh
@@ -33,8 +33,8 @@ cat toxav/rtp.c |grep 'define DISABLE_H264_ENCODER_FEATURE'
 ./autogen.sh
 make clean
 export CFLAGS_=" $CF2 -D_GNU_SOURCE -I$_INST_/include/ -O3 -g -fstack-protector-all "
-export CFLAGS="$CFLAGS"
-export CFLAGS=" $CFLAGS -Werror=div-by-zero -Werror=format=2 -Werror=implicit-function-declaration "
+export CFLAGS="$CFLAGS_"
+export CFLAGS=" $CFLAGS -Wformat=0 -Werror=div-by-zero -Werror=format=2 -Werror=implicit-function-declaration "
 export LDFLAGS=-L$_INST_/lib
 
 ./configure \
