a7ad1c174adf0e2604561cd81e5aac0502bf4aaf
zoff99 <zoff@zoff.cc>
2020-03-13 17:18:26 +0100

    apidsl

--- toxcore/tox.h
+++ toxcore/tox.h
@@ -3271,6 +3271,7 @@ void tox_callback_friend_lossy_packet_per_pktid(Tox *tox, tox_friend_lossy_packe
  */
 void tox_callback_friend_lossless_packet_per_pktid(Tox *tox, tox_friend_lossless_packet_cb *callback, uint8_t pktid);
 
-
+void tox_set_av_object(Tox *tox, const void *object);
+void tox_get_av_object(const Tox *tox, void **object);
 
 #endif // C_TOXCORE_TOXCORE_TOX_H
