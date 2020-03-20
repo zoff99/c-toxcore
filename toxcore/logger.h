94b3974713f4573ecffcdbe1e7fd9d016389f95e
zoff99 <zoff@zoff.cc>
2020-03-20 20:56:30 +0100

    remove gnu def

--- toxcore/logger.h
+++ toxcore/logger.h
@@ -86,7 +86,7 @@ void logger_write(
     const char *format, ...) GNU_PRINTF(6, 7);
 
 void logger_api_write(const Logger *log, Logger_Level level, const char *file, int line, const char *func,
-                      const char *format, va_list args); GNU_PRINTF(6, 7);
+                      const char *format, va_list args);
 
 
 /**
