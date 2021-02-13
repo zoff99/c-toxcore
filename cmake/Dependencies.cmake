4a9d1879fbba6bb99cbac6674620801f69ae19ed
zoff99 <zoff99@users.noreply.github.com>
2021-02-13 11:06:46 +0100

    update from upstream

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -41,18 +41,20 @@ pkg_use_module(MSGPACK              msgpack      )
 if(MSVC)
   # libsodium
   # ---------
-  find_library(LIBSODIUM_LIBRARIES
-    NAMES sodium libsodium
-    PATHS
-      "third_party/libsodium/Win32/Release/v140/dynamic"
-      "third_party/libsodium/x64/Release/v140/dynamic"
-  )
-  if(LIBSODIUM_LIBRARIES)
-    include_directories("third_party/libsodium/include")
-    set(LIBSODIUM_FOUND TRUE)
-    message("libsodium: ${LIBSODIUM_LIBRARIES}")
-  else()
-    message(FATAL_ERROR "libsodium libraries not found")
+  if(NOT LIBSODIUM_FOUND)
+    find_library(LIBSODIUM_LIBRARIES
+      NAMES sodium libsodium
+      PATHS
+        "third_party/libsodium/Win32/Release/v140/dynamic"
+        "third_party/libsodium/x64/Release/v140/dynamic"
+    )
+    if(LIBSODIUM_LIBRARIES)
+      include_directories("third_party/libsodium/include")
+      set(LIBSODIUM_FOUND TRUE)
+      message("libsodium: ${LIBSODIUM_LIBRARIES}")
+    else()
+      message(FATAL_ERROR "libsodium libraries not found")
+    endif()
   endif()
 
   # pthreads
@@ -69,7 +71,12 @@ if(MSVC)
       add_definitions(-DHAVE_STRUCT_TIMESPEC)
       message("libpthreads: ${CMAKE_THREAD_LIBS_INIT}")
     else()
-      message(FATAL_ERROR "libpthreads libraries not found")
+      find_package(pthreads4w)
+      if(NOT pthreads4w_FOUND)
+        message(FATAL_ERROR "libpthreads libraries not found")
+      endif()
+      include_directories(${pthreads4w_INCLUDE_DIR})
+      link_libraries(${pthreads4w_LIBRARIES})
     endif()
   endif()
 endif()
