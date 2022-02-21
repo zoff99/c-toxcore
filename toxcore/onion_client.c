6e6eee66481b732056187217080fa71f4fa5d965
jfreegman <jfreegman@gmail.com>
2022-02-21 16:04:36 -0500

    Fix issue with friend finding taking too long after disconnects
    This forces the onion to consider all friends as new after a 10 second
    period of the onion being disconnected. new friends are aggressively
    searched for in the DHT, whereas old friends are only searched for in
    intervals spanning minutes.

--- toxcore/onion_client.c
+++ toxcore/onion_client.c
@@ -114,6 +114,7 @@ struct Onion_Client {
     uint8_t secret_symmetric_key[CRYPTO_SYMMETRIC_KEY_SIZE];
     uint64_t last_run;
     uint64_t first_run;
+    uint64_t last_time_connected;
 
     uint8_t temp_public_key[CRYPTO_PUBLIC_KEY_SIZE];
     uint8_t temp_secret_key[CRYPTO_SECRET_KEY_SIZE];
@@ -1792,7 +1793,20 @@ static int onion_isconnected(const Onion_Client *onion_c)
     return 0;
 }
 
+non_null()
+static void reset_friend_run_counts(Onion_Client *onion_c)
+{
+    for (uint16_t i = 0; i < onion_c->num_friends; ++i) {
+        Onion_Friend *o_friend = &onion_c->friends_list[i];
+
+        if (o_friend->status != 0) {
+            o_friend->run_count = 0;
+        }
+    }
+}
+
 #define ONION_CONNECTION_SECONDS 3
+#define ONION_CONNECTED_TIMEOUT 10
 
 /*  return 0 if we are not connected to the network.
  *  return 1 if we are connected with TCP only.
@@ -1823,7 +1837,13 @@ void do_onion_client(Onion_Client *onion_c)
         do_announce(onion_c);
     }
 
+    if (mono_time_is_timeout(onion_c->mono_time, onion_c->last_time_connected, ONION_CONNECTED_TIMEOUT)) {
+        reset_friend_run_counts(onion_c);
+    }
+
     if (onion_isconnected(onion_c)) {
+        onion_c->last_time_connected = mono_time_get(onion_c->mono_time);
+
         if (onion_c->onion_connected < ONION_CONNECTION_SECONDS * 2) {
             ++onion_c->onion_connected;
         }
