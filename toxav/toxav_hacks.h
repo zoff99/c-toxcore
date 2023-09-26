e66939e8ccbdda770e94a2eb99bc69afb3cea9dc
zoff99 <zoff99@users.noreply.github.com>
2023-09-26 12:25:18 +0200

    fix cimple_test: tokstyle violations detected

--- toxav/toxav_hacks.h
+++ toxav/toxav_hacks.h
@@ -16,15 +16,20 @@ typedef struct ToxAVCall ToxAVCall;
 
 non_null(1)
 ToxAVCall *call_get(ToxAV *av, uint32_t friend_number);
+
 non_null(1)
 RTPSession *rtp_session_get(ToxAVCall *call, int payload_type);
-non_null(1)
+
+non_null()
 MSISession *tox_av_msi_get(ToxAV *av);
-non_null(1)
+
+non_null()
 BWController *bwc_controller_get(ToxAVCall *call);
-non_null(1)
+
+non_null()
 Mono_Time *toxav_get_av_mono_time(ToxAV *av);
-non_null(1)
+
+non_null()
 Logger *toxav_get_logger(ToxAV *av);
 
 #endif // C_TOXCORE_TOXAV_HACKS_H
