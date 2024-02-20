8bcb794f4b128e0fb3ac1f41cc1e90fd63eabd3d
zoff99 <zoff99@users.noreply.github.com>
2024-02-20 16:32:36 +0100

    fix: [FreeBSD] failed to set socket option 4098 see https://github.com/TokTok/c-toxcore/issues/2306

--- toxcore/network.c
+++ toxcore/network.c
@@ -1181,6 +1181,7 @@ Networking_Core *new_networking_ex(
      */
     int n = 1024 * 1024 * 2;
 
+#if !defined(__FreeBSD__)
     if (net_setsockopt(ns, temp->sock, SOL_SOCKET, SO_RCVBUF, &n, sizeof(n)) != 0) {
         LOGGER_ERROR(log, "failed to set socket option %d", SO_RCVBUF);
     }
@@ -1188,6 +1189,7 @@ Networking_Core *new_networking_ex(
     if (net_setsockopt(ns, temp->sock, SOL_SOCKET, SO_SNDBUF, &n, sizeof(n)) != 0) {
         LOGGER_ERROR(log, "failed to set socket option %d", SO_SNDBUF);
     }
+#endif
 
     /* Enable broadcast on socket */
     int broadcast = 1;
