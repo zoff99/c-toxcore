7249387edaf19b3cc4a19ee897879ed518479b9f
zoff99 <zoff99@users.noreply.github.com>
2024-12-11 18:25:06 +0100

    XX DEBUG

--- amalgamation/toxcore_amalgamation.c
+++ amalgamation/toxcore_amalgamation.c
@@ -82920,6 +82920,7 @@ int vc_reconfigure_encoder_h265(Logger *log, VCSession *vc, uint32_t bit_rate,
             (vc->h265_enc_height == height) &&
             (vc->h264_enc_bitrate != bit_rate))
     {
+#if 0
         // HINT: just bitrate has changed
         // LOGGER_API_WARNING(vc->av->tox, "vc_reconfigure_encoder_h265:1:bit_rate = %d vc->h264_enc_bitrate = %d", (int)bit_rate, (int)vc->h264_enc_bitrate);
         x265_param *param = x265_param_alloc();
@@ -82930,8 +82931,23 @@ int vc_reconfigure_encoder_h265(Logger *log, VCSession *vc, uint32_t bit_rate,
         param->rc.vbvMaxBitrate = ((int)(bit_rate / 1000)) - 1;
 
         int res = x265_encoder_reconfig(vc->h265_encoder, param);
-        printf("x265 [****] x265_encoder_reconfig:res=%d bitrate=%d\n", (int)res, (int)(bit_rate / 1000));
         x265_param_free(param);
+        setvbuf(stdout, NULL, _IOLBF, 0);
+        setvbuf(stderr, NULL, _IOLBF, 0);
+        printf("x265 [****] x265_encoder_reconfig:res=%d bitrate=%d\n", (int)res, (int)(bit_rate / 1000));
+#else
+        // HINT: more has changed do a full encoder shutdown and re-init
+        vc_kill_encoder_h265(vc);
+        vc->h265_enc_height = height;
+        vc->h265_enc_width = width;
+        // LOGGER_API_WARNING(vc->av->tox, "vc_reconfigure_encoder_h265:2:bit_rate = %d vc->h264_enc_bitrate = %d", (int)bit_rate, (int)vc->h264_enc_bitrate);
+        vc_init_encoder_h265(log, vc, bit_rate, width, height);
+
+        setvbuf(stdout, NULL, _IOLBF, 0);
+        setvbuf(stderr, NULL, _IOLBF, 0);
+        printf("x265 [****] x265_encoder_reconfig:bitrate=%d\n", (int)(bit_rate / 1000));
+
+#endif
     }
     else
     {
