59e533f9e42c3cfced318ee636e565ac77842331
zoff99 <zoff99@users.noreply.github.com>
2026-09-25 20:10:50 +0200

    fix the auto_tests

--- auto_tests/onion_test.c
+++ auto_tests/onion_test.c
@@ -471,7 +471,7 @@ static Onions *new_onions(const Random *rng, uint16_t port, uint32_t *index)
     }
 
     TCP_Proxy_Info inf = {{{{0}}}};
-    on->onion_c = new_onion_client(on->log, rng, on->mono_time, new_net_crypto(on->log, rng, ns, on->mono_time, dht, &inf));
+    on->onion_c = new_onion_client(on->log, rng, on->mono_time, new_net_crypto(on->log, rng, ns, on->mono_time, dht, &inf, NULL));
 
     if (!on->onion_c) {
         kill_onion_announce(on->onion_a);
